87840fe99376c2150b74cf20848808195e5a1edc
Tyler Williams <tyler.williams2996@gmail.com>
2020-06-08 02:58:03 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,13 +1,13 @@
 
 
 def speak_to_grandma(string)
-   uppercase = string.upcase
-   lowercase = string.downcase
+   uppercase = "HELLO"
+   lowercase = "hello"
    love = "I LOVE YOU GRANDMA!"
   
   if love
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif string == 
+  elsif uppercase
     return "NO, NOT SINCE 1938!"
   elsif lowercase
     return "HUH?! SPEAK UP, SONNY!"
